01c0e5dafe858c5b0f9cc177cc610bb05e2d5e29
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-06 02:17:14 -0500

    Implement POST request handling

---
+++ api/create.php
@@ -0,0 +1,31 @@
+<?php
+ include("../config/config.php");
+
+ $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+    $json_data = file_get_contents('php://input');
+    $post_data = json_decode($json_data, true);
+
+    $nombre = $post_data['nombre'];
+    $email = $post_data['email'];
+
+    // $nombre = $_POST["nombre"];
+    // $email = $_POST["email"];
+
+    $query = "INSERT INTO usuarios (nombre, email) VALUES ('$nombre', '$email')";
+    $result = mysqli_query($conn, $query);
+
+    if($result) {
+        echo json_encode(array("message" => "Usuario creado con exito"));
+    } else {
+        echo json_encode(array("error" => "Error al crear usuario"));
+    }
+} else {
+    echo json_encode(array("error" => "Metodo no permitido."));
+}
+
+mysqli_close($conn);
+
+?>
