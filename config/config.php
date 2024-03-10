6e46774df9448bb1e087697ddedaf403f8f2d871
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-09 21:26:03 -0500

    Implement environment variables to database connection

--- config/config.php
+++ config/config.php
@@ -1,6 +1,11 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "AD123min_?");
-define("DB_NAME", "crud_demo");
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../");
+$dotenv->load();
+
+define("DB_HOST", $_ENV['DB_HOST']);
+define("DB_USERNAME", $_ENV['DB_USERNAME']);
+define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
+define("DB_NAME", $_ENV['DB_NAME']);
 ?>
