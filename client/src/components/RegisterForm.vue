bd44c4ad540b125bd5b489db39f057e363692b32
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-17 20:48:19 -0500

    Add component to register users in a form

---
+++ client/src/components/RegisterForm.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const name = ref("");
+const age = ref();
+const email = ref("");
+
+async function createUser() {
+    const URL = "http://localhost/crud-php-vue/api/create.php";
+    const data = {
+        nombre: name.value,
+        edad: age.value,
+        email: email.value
+    }
+    const response = await fetch(URL, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+
+    // Limpiar los campos del formulario después de enviarlos
+    name.value = "";
+    age.value = "";
+    email.value = "";
+}
+
+
+</script>
+<template>
+    <form @submit.prevent="createUser">
+        <label for="GET-name">Nombre:</label>
+        <input id="GET-name" v-model="name" type="text" name="name" required/>
+        <label for="GET-age">Edad:</label>
+        <input id="GET-age" v-model="age" type="number" name="age" />
+        <label for="GET-email">Email:</label>
+        <input id="GET-email" v-model="email" type="email" name="email" required />
+        <input type="submit" value="Save" />
+    </form>
+
+    <div id="preview-card-user">
+        <p>Nombre de Usuario: {{ name }}</p>
+        <p>Edad: {{ age }}</p>
+        <p>Email: {{ email }}</p>
+    </div>
+
+</template>
