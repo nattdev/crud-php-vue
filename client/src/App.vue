e32229d874043114ad73aabaabd985b9e630a3ac
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-17 20:51:20 -0500

    Add nav and router view to the main page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { RouterView } from 'vue-router';
+
+import { useRoute, useRouter } from 'vue-router'
+
+const location = useRoute();
+const router = useRouter();
+
 </script>
 
 <template>
+  <div>
+    <h1>Hello App!</h1>
+    <router-link to="/">Home</router-link><br>
+    <router-link to="/register">Registrar Usuario</router-link><br>
+    <router-link to="/users">Ver Usuarios</router-link><br>
+    <p>
+      <strong>Current route path:</strong> {{ $route.fullPath }}
+      <strong>Current route path:</strong> {{ $route.params.id }}
+    </p>
+  </div>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
