6d0960c6a2bbf7f44ee79036082516640ca73700
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-20 01:58:38 -0500

    Implement endpoint with id parameter to show a single user

--- api/read.php
+++ api/read.php
@@ -1,18 +1,28 @@
 <?php
-    include("../config/config.php");
+include("../config/config.php");
 
-    $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = explode( '/', $uri );
+
+$id = $uri[4];
+
+if ($id) {
+    $query = "SELECT * FROM usuarios where id = $id";
+} else {
     $query = "SELECT * FROM usuarios";
+}
 
-    $result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
-    $usuarios = array();
+$usuarios = array();
 
-    while($row = mysqli_fetch_assoc($result)) {
-        $usuarios[] = $row;
-    }
+while ($row = mysqli_fetch_assoc($result)) {
+    $usuarios[] = $row;
+}
 
-    echo(json_encode($usuarios));
+echo (json_encode($usuarios));
 
-    mysqli_close($conn);
-?>
+mysqli_close($conn);
