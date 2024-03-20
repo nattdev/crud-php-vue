a58399961d9cea7386f9ace49a6a520289c2c6c3
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-20 16:31:28 -0500

    Implement a function that returns the specific title based on the path

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, provide } from 'vue';
-import { RouterView } from 'vue-router';
+import { useRoute, RouterView } from 'vue-router';
+
+const route = useRoute();
 
 const users = ref([]);
 provide('users', { users, getUsers });
@@ -13,24 +15,38 @@ async function getUsers() {
 }
 
 getUsers();
+
+function pathName() {
+  if(route.fullPath == '/') {
+    return "CRUD-PHP-VUE";
+  } else if(route.fullPath == '/register') {
+    return "REGISTRAR USUARIO";
+  } else if(route.fullPath == `/users/${route.params.id}`) {
+    return "ACTUALIZAR USUARIO";
+  } else {
+    return "USUARIOS";
+  }
+}
+
 </script>
 
 <template>
   <div class="p-5">
     <div class="flex items-center">
-      <h1 class="text-2xl font-semibold mr-4 min-w-56">{{$route.fullPath == '/' ? "CRUD-PHP-VUE": "USUARIOS"}}</h1>
+      <h1 class="text-2xl font-semibold mr-4 min-w-56">{{pathName()}}</h1>
       <nav class="flex flex-row p-4 gap-6 text-stone-500">
         <router-link to="/">Home</router-link>
         <router-link to="/register">Registrar Usuario</router-link>
         <router-link to="/users">Ver Usuarios</router-link>
       </nav>
     </div>
-    <main>
+    <main class="mt-6">
       <RouterView />
     </main>
     <footer class="bg-slate-50">
-      <div class="dev-info flex gap-3 font-light">
-          <p>Current route path: <span class="font-semibold">{{ $route.fullPath }} </span></p>
+      <div class="dev-info flex font-light absolute bottom-0 flex-wrap">
+          <p class="w-full font-semibold">Dev info:</p>
+          <p>Current route path: <span class="font-semibold pr-6">{{ $route.fullPath }} </span></p>
           <p>Current params route path: <span class="font-semibold">{{ $route.params.id }} </span></p>
       </div>
     </footer>
