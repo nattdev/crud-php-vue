70127b562ff2b70d89b07d0099fa943c2c1cff7e
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-20 11:52:04 -0500

    Implement updateUser function

--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { inject, ref } from 'vue';
+import { inject, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
-const {getUsers} = inject('users');
-console.log(getUsers);
+const { getUsers } = inject('users');
 
 const name = ref("");
 const age = ref();
 const email = ref("");
 
+const route = useRoute();
+
 async function createUser() {
+
     const URL = "http://localhost/crud-php-vue/api/create.php";
     const data = {
         nombre: name.value,
@@ -23,31 +25,71 @@ async function createUser() {
             "Content-Type": "application/json",
         },
     })
-
-    // Limpiar los campos del formulario después de enviarlos
-    name.value = "";
-    age.value = "";
-    email.value = "";
-
+    clearFields();
     getUsers();
 }
 
-const route = useRoute();
+async function updateUser() {
+    const URL = "http://localhost/crud-php-vue/api/update.php";
+    const data = {
+        id: parseInt(route.params.id),
+        nombre: name.value,
+        email: email.value
+    }
+    const response = await fetch(URL, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    getUsers();
+}
 
-if(route.params.id) {
-    console.log("update");
+function submitForm() {
+    if (route.params.id) {
+        updateUser();
+    } else {
+        createUser();
+    }
+}
+
+async function loadUser() {
+    const URL = `http://localhost/crud-php-vue/api/read.php/${route.params.id}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    name.value = data[0].nombre;
+    age.value = data[0].edad;
+    email.value = data[0].email;
+}
+
+function clearFields() {
+    name.value = "";
+    age.value = "";
+    email.value = "";
+}
+
+watch(() => route.path, (newPath, oldPath) => {
+    if (newPath === '/register') {
+        clearFields();
+    }
+});
+
+if (route.params.id) {
+    loadUser();
 }
 
 </script>
 <template>
 
-    <h2>{{ $route.params.id ? "Update User" : "Create User"}}</h2>
+    <h2>{{ $route.params.id ? "Update User" : "Create User" }}</h2>
 
-    <form @submit.prevent="createUser">
+    <form @submit.prevent="submitForm">
         <label for="GET-name">Nombre:</label>
-        <input id="GET-name" v-model="name" type="text" name="name" required/>
-        <label for="GET-age">Edad:</label>
-        <input id="GET-age" v-model="age" type="number" name="age" />
+        <input id="GET-name" v-model="name" type="text" name="name" required />
+        <label for="GET-age" v-if="!$route.params.id">Edad:</label>
+        <input id="GET-age" v-model="age" type="number" name="age" v-if="!$route.params.id"/>
         <label for="GET-email">Email:</label>
         <input id="GET-email" v-model="email" type="email" name="email" required />
         <input type="submit" value="Save" />
