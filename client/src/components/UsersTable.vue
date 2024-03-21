a40befc125c858cff189a70b996c5ddec374f510
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-21 18:09:45 -0500

    Add header of users table and tailwind styles

--- client/src/components/UsersTable.vue
+++ client/src/components/UsersTable.vue
@@ -30,12 +30,27 @@ function navigateToUpdateUser(id) {
 </script>
 
 <template>
-    <div v-for="user in users">
-        <p>ID: {{ user.id }}</p>
-        <p>Nombre: {{ user.nombre }}</p>
-        <p>Edad: {{ user.edad }}</p>
-        <p>Email: {{ user.email }}</p>
-        <button @click="deleteUser(user.id)">DELETE</button>
-        <button @click="navigateToUpdateUser(user.id)">UPDATE</button>
+    <div class="w-full">
+        <div class="text-left text-lg hidden sm:grid sm:grid-cols-[1fr_2fr_1fr_2fr_2fr] border-y border-slate-300 font-light my-2">
+            <p>ID</p>
+            <p>Nombre</p>
+            <p>Edad</p>
+            <p>Email</p>
+            <p>Acciones</p>
+        </div>
+        <div class=" bg-[#FBFCFF] flex flex-col sm:p-0 sm:py-2 p-3 my-2 rounded-xl lg:grid md:grid-cols-[1fr_2fr_1fr_2fr_2fr] gap-y-2  relative break-words" v-for="user in users">
+            <p class="absolute right-0 pr-3 lg:relative lg:pr-0">{{ user.id }}</p>
+            <p class=" font-semibold sm:text-lg text-xl">{{ user.nombre }}</p>
+            <p class="">{{ user.edad }} años</p>
+            <p class="">{{ user.email }}</p>
+            <p class="min-w-72">
+                <button
+                    class="mr-5 px-3 border-2 rounded-3xl w-fit cursor-pointer border-green-500 text-green-500 font-semibold text-base"
+                    @click="navigateToUpdateUser(user.id)">EDITAR</button>
+                <button
+                    class="mr-5 px-3 border-2 rounded-3xl w-fit cursor-pointer border-orange-700 text-orange-700 font-semibold text-base"
+                    @click="deleteUser(user.id)">ELIMINAR</button>
+            </p>
+        </div>
     </div>
 </template>
