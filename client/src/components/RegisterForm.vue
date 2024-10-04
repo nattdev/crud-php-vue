4ded5d22bfc3a195428a1ec5b7adc3792bd7ec06
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-10-04 00:38:48 -0500

    Implement timeout for success message display

--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -14,6 +14,7 @@ const route = useRoute();
 
 const loading = ref(false);
 const successMessage = ref("");
+const showSuccessMessage = ref(false);
 
 async function createUser() {
     const URL = `${HOST}/crud-php-vue/api/create.php`;
@@ -58,18 +59,22 @@ async function submitForm() {
             const response = await createUser();
             response.status == 201 ? successMessage.value = "El usuario fue creado exitosamente" : "";
         }
-        
+        showSuccessMessage.value = true;
+
     } catch (error) {
         console.error('Error al crear/actualizar el usuario:', error);
         successMessage.value = 'Error al guardar el usuario';
     } finally {
         loading.value = false;
-            if(route.params.id) {
-                getUsers();
-            } else {
-                clearFields();
-                getUsers();
-            }
+        setTimeout(() => {
+            showSuccessMessage.value = false;
+        }, 2500);
+        if (route.params.id) {
+            getUsers();
+        } else {
+            clearFields();
+            getUsers();
+        }
     }
 }
 
@@ -102,19 +107,28 @@ if (route.params.id) {
 </script>
 <template>
     <div class="bg-[#FBFCFF] rounded-xl flex items-center flex-col sm:px-12 py-6 md:flex-row">
-        <form class="flex flex-col pr-9 py-6 text-lg flex-grow gap-2 w-full md:w-fit leading-tight" @submit.prevent="submitForm">
+        <form class="flex flex-col pr-9 pb-6 px-3 text-lg flex-grow gap-2 w-full md:w-fit leading-none"
+            @submit.prevent="submitForm">
             <label class="font-semibold p-1" for="GET-name">Nombre</label>
-            <input class="p-1 border-b-slate-200 border-b-2" id="GET-name" v-model="name" type="text" name="name" required />
+            <input class="p-1 border-b-slate-200 border-b-2" id="GET-name" v-model="name" type="text" name="name"
+                required />
             <label class="font-semibold p-1" for="GET-age" v-if="!$route.params.id">Edad</label>
-            <input class="p-1 border-b-slate-200 border-b-2" id="GET-age" v-model="age" type="number" name="age" v-if="!$route.params.id" />
+            <input class="p-1 border-b-slate-200 border-b-2" id="GET-age" v-model="age" type="number" name="age"
+                v-if="!$route.params.id" />
             <label class="font-semibold p-1" for="GET-email">Email</label>
-            <input class="p-1 border-b-slate-200 border-b-2" id="GET-email" v-model="email" type="email" name="email" required />
-            <input class="py-1 px-6 mt-3 border-2 rounded-3xl w-fit cursor-pointer border-slate-600 text-slate-600 font-semibold text-base hover:bg-slate-600 hover:text-white transition-colors disabled:bg-slate-200 disabled:border-slate-200 disabled:text-slate-300 disabled:hover:bg-none disabled:hover:text-slate-300"  type="submit" value="GUARDAR" :disabled="loading"/><span>{{ successMessage }}</span>
+            <input class="p-1 border-b-slate-200 border-b-2" id="GET-email" v-model="email" type="email" name="email"
+                required />
+            <input
+                class="py-1 px-6 mt-3 border-2 rounded-3xl w-fit cursor-pointer border-slate-600 text-slate-600 font-semibold text-base hover:bg-slate-600 hover:text-white transition-colors disabled:bg-slate-200 disabled:border-slate-200 disabled:text-slate-300 disabled:hover:bg-none disabled:hover:text-slate-300"
+                type="submit" value="GUARDAR" :disabled="loading" /><span v-show="showSuccessMessage"
+                :class="{ 'text-green-700 font-medium': successMessage && successMessage.includes('creado'), 'text-amber-700 font-medium': successMessage && successMessage.includes('actualizado') }">{{
+                successMessage }}</span>
         </form>
-        <div id="preview-card-user" class="flex overflow-hidden flex-col justify-center h-fit p-6 items-center gap-2 bg-white shadow-md rounded-xl md:w-2/5 w-full break-words text-center mb-4 sm:mb-0">
+        <div id="preview-card-user"
+            class="flex overflow-hidden flex-col justify-center h-fit p-6 items-center gap-2 bg-white shadow-md rounded-xl md:w-2/5 w-full break-words text-center mb-4 sm:mb-0">
             <p class="font-semibold mb-2">Nuevo Usuario</p>
-            <img class="min-w-16 min-h-5 rounded-full" src="../assets/avatar/avatar.svg"/>
-            <p class="mt-3 min-h-6 max-w-full max-h-12 text-xl overflow-y-auto">{{ name }}</p>
+            <img class="min-w-16 min-h-5 rounded-full" src="../assets/avatar/avatar.svg" />
+            <p class="mt-3 min-h-6 max-w-full max-auto text-xl overflow-y-auto">{{ name }}</p>
             <p class="min-h-6 max-w-full max-h-12 overflow-y-auto">{{ age ? `${age} años` : "" }}</p>
             <p class="min-h-6 max-w-full max-h-12 overflow-y-auto">{{ email }}</p>
         </div>
